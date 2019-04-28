28ae67b4f28381ea317abb9d75a6f074d9065b67
guojiaxing <302802003@qq.com>
2019-04-28 20:53:07 +0800

    更新授权装饰器，装饰器可传参数，参数为需要授权的权限个数

--- util/permission_exception_hand.py
+++ util/permission_exception_hand.py
@@ -13,32 +13,36 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from util.get_by_local import GetByLocal
 
-
-def authorize(func):
+def authorize(num=1):
     """
     装饰器的作用是对在需要授权的地方做容错处理，进行授权。被装饰方法的当前实例对象必须要有driver属性
     args[0]  这里是self
-    :param func:
+    :param num:需要授予的权限个数
     :return:
-    """
-    def wrapper(*args,**kwargs):
-        try:
-            func(*args,**kwargs)
-        except Exception:
-            getByLocal = GetByLocal(args[0].driver)
-            #判断授权按钮是否可见，如果存在则点击授权按钮
+     """
+    def middle(func):
+        def wrapper(*args,**kwargs):
             try:
-                allow_button_a = WebDriverWait(args[0], 3, 0.1).until(EC.visibility_of(getByLocal.get_element('login_element','allowButtonA')))
-                allow_button_a.click() if allow_button_a else ''
+                func(*args,**kwargs)
             except Exception:
-                pass
-            try:
-                allow_button_b = WebDriverWait(args[0], 3, 0.1).until(EC.visibility_of(getByLocal.get_element('login_element', 'allowButtonB')))
-                allow_button_b.click() if allow_button_a else ''
-            except Exception:
-                pass
+                getByLocal = GetByLocal(args[0].driver)
+                #判断授权按钮是否可见，如果存在则点击授权按钮
+                for n in range(num):
+                    try:
+                        allow_button_a = WebDriverWait(args[0], 2, 0.1).until(EC.visibility_of(getByLocal.get_element('login_element','allowButtonA')))
+                        allow_button_a.click() if allow_button_a else ''
+                    except Exception:
+                        pass
+                for n in range(num):
+                    try:
+                        allow_button_b = WebDriverWait(args[0], 2, 0.1).until(EC.visibility_of(getByLocal.get_element('login_element', 'allowButtonB')))
+                        allow_button_b.click() if allow_button_a else ''
+                    except Exception:
+                        pass
 
-            time.sleep(3)
-            func(*args, **kwargs)
+                time.sleep(2)
+                func(*args, **kwargs)
 
-    return wrapper
+        return wrapper
+
+    return middle
